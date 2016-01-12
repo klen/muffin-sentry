ed3a932aa3b3a821f2dc4c5c77fbf38816fffdb1
Kirill Klenov <horneds@gmail.com>
2016-01-12 10:36:15 +0300

    Bump version: 0.1.3 → 0.1.4

--- muffin_sentry.py
+++ muffin_sentry.py
@@ -12,7 +12,7 @@ from muffin.plugins import BasePlugin
 # Package information
 # ===================
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __project__ = "muffin-sentry"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
