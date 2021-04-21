68ed14e4f2d20d2c723bc9e39bc90e8d1c4669cb
Kirill Klenov <horneds@gmail.com>
2021-04-21 21:18:30 +0300

    Bump version: 0.5.6 → 0.5.7

--- muffin_sentry/__init__.py
+++ muffin_sentry/__init__.py
@@ -12,7 +12,7 @@ from sentry_sdk import init as sentry_init, Hub as SentryHub, Scope as SentrySco
 # Package information
 # ===================
 
-__version__ = "0.5.6"
+__version__ = "0.5.7"
 __project__ = "muffin-sentry"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
